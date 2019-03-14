b0db77e916147828305024a0a23c9bede0368592
Diogo Cunha <diogofncunha@gmail.com>
2019-03-14 16:39:10 +0000

    feat: 🎸 added ReactPersistRoute component
    Added the base component that listens for route changes for any router
    and saved them to any possible location

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,7 +1,272 @@
 import React from "react";
-import { render } from "react-testing-library";
-import ReactPersistRoute from "..";
+import { render, cleanup } from "react-testing-library";
+import ReactPersistRoute, {
+  Props,
+  LocationListener,
+  History,
+  SerializableLocation
+} from "..";
+import URL from "url";
 
-test("should render", () => {
-  render(<ReactPersistRoute />);
+describe("<ReactPersistRoute />", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(cleanup);
+
+  const setup = () => {
+    const toEmit: LocationListener[] = [];
+
+    const emit = (location: Location) => {
+      toEmit.forEach(te => te(location, "PUSH"));
+    };
+
+    const unlisten = jest.fn();
+    const key = "mypwa";
+
+    const history: History = {
+      listen: (f: LocationListener) => {
+        toEmit.push(f);
+
+        return unlisten;
+      },
+      push: jest.fn()
+    };
+
+    const props: Props<History> = {
+      history,
+      adapter: {
+        onClear: jest.fn(),
+        onLoad: jest.fn(),
+        onSave: jest.fn(),
+        getKey: () => key
+      }
+    };
+
+    return {
+      props,
+      key,
+      toEmit,
+      emit,
+      unlisten
+    };
+  };
+
+  const getLocation = (urlStr: string): Location => {
+    const url = URL.parse(urlStr);
+
+    return {
+      ancestorOrigins: {} as any,
+      hash: url.hash || "",
+      host: url.host || "",
+      hostname: url.hostname || "",
+      origin: "",
+      href: url.href || "",
+      pathname: url.pathname || "",
+      port: url.port || "",
+      search: url.search || "",
+      protocol: url.protocol || "",
+      assign: jest.fn(),
+      reload: jest.fn(),
+      replace: jest.fn()
+    };
+  };
+
+  describe("when component loads", () => {
+    test("when component unmounts before loading key should generate a no-op", () => {
+      const { props, unlisten, toEmit } = setup();
+      const { unmount } = render(<ReactPersistRoute {...props} />);
+
+      unmount();
+
+      jest.runAllTimers();
+
+      expect(props.adapter.onLoad).toHaveBeenCalled();
+      expect(unlisten).not.toHaveBeenCalled();
+      expect(toEmit.length).toBe(0);
+      expect(props.history.push).not.toHaveBeenCalled();
+    });
+
+    describe("when a saved location is not loaded", () => {
+      test("should not push history", () => {
+        const { props } = setup();
+
+        render(<ReactPersistRoute {...props} />);
+
+        jest.runAllTimers();
+
+        expect(props.adapter.onLoad).toHaveBeenCalled();
+        expect(props.history.push).not.toHaveBeenCalled();
+      });
+
+      test("should open listen connection", () => {
+        const { props, toEmit } = setup();
+
+        render(<ReactPersistRoute {...props} />);
+
+        expect(toEmit.length).toBe(0);
+        jest.runAllTimers();
+        expect(toEmit.length).toBe(1);
+      });
+
+      test("should not call unlisten when component is not unmounted", () => {
+        const { props, unlisten, toEmit } = setup();
+
+        render(<ReactPersistRoute {...props} />);
+
+        expect(toEmit.length).toBe(0);
+        jest.runAllTimers();
+        expect(unlisten).not.toHaveBeenCalled();
+      });
+    });
+
+    describe("when a saved location is loaded", () => {
+      test("should push to history", () => {
+        const { props, key } = setup();
+
+        (props.adapter.onLoad as jest.Mock<any, any>).mockImplementation(
+          (k: string): SerializableLocation => {
+            if (k === key) {
+              return {
+                hash: "",
+                host: "mypwa.com",
+                hostname: "mypwa.com",
+                href: "https://mypwa.com/u/1",
+                origin: "",
+                pathname: "/u/1",
+                port: "",
+                protocol: "https:",
+                search: ""
+              };
+            }
+
+            throw new Error("Ups wrong key");
+          }
+        );
+
+        render(<ReactPersistRoute {...props} />);
+
+        jest.runAllTimers();
+
+        expect(props.history.push).toHaveBeenCalledWith(
+          "https://mypwa.com/u/1"
+        );
+      });
+
+      test("should open listen connection", () => {
+        const { props, key, toEmit } = setup();
+
+        (props.adapter.onLoad as jest.Mock<any, any>).mockImplementation(
+          (k: string): SerializableLocation => {
+            if (k === key) {
+              return {
+                hash: "",
+                host: "mypwa.com",
+                hostname: "mypwa.com",
+                href: "https://mypwa.com/u/1",
+                origin: "",
+                pathname: "/u/1",
+                port: "",
+                protocol: "https:",
+                search: ""
+              };
+            }
+
+            throw new Error("Ups wrong key");
+          }
+        );
+
+        render(<ReactPersistRoute {...props} />);
+
+        expect(toEmit.length).toBe(0);
+        jest.runAllTimers();
+        expect(toEmit.length).toBe(1);
+      });
+
+      test("should not call unlisten when component is not unmounted", () => {
+        const { props, key, unlisten } = setup();
+
+        (props.adapter.onLoad as jest.Mock<any, any>).mockImplementation(
+          (k: string): SerializableLocation => {
+            if (k === key) {
+              return {
+                hash: "",
+                host: "mypwa.com",
+                hostname: "mypwa.com",
+                href: "https://mypwa.com/u/1",
+                origin: "",
+                pathname: "/u/1",
+                port: "",
+                protocol: "https:",
+                search: ""
+              };
+            }
+
+            throw new Error("Ups wrong key");
+          }
+        );
+
+        jest.runAllTimers();
+        expect(unlisten).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe("when route changes", () => {
+    test("should call adpater's onSave with the serialized location for basic urls", () => {
+      const { props, emit } = setup();
+
+      render(<ReactPersistRoute {...props} />);
+
+      jest.runAllTimers();
+
+      // Change route.
+      emit(getLocation("https://mypwa.com/u/1"));
+      jest.runAllTimers();
+
+      expect(props.adapter.onSave).toMatchSnapshot();
+    });
+
+    test("should call adpater's onSave with the serialized location for urls with search", () => {
+      const { props, emit } = setup();
+
+      render(<ReactPersistRoute {...props} />);
+
+      jest.runAllTimers();
+
+      // Change route.
+      emit(getLocation("https://mypwa.com/u/1?q=getit"));
+      jest.runAllTimers();
+
+      expect(props.adapter.onSave).toMatchSnapshot();
+    });
+
+    test("should call adpater's onSave with the serialized location for urls with port", () => {
+      const { props, emit } = setup();
+
+      render(<ReactPersistRoute {...props} />);
+
+      jest.runAllTimers();
+
+      // Change route.
+      emit(getLocation("https://mypwa:8080/u/1"));
+      jest.runAllTimers();
+
+      expect(props.adapter.onSave).toMatchSnapshot();
+    });
+
+    test("should call adpater's onSave with the serialized location for urls with hash", () => {
+      const { props, emit } = setup();
+
+      render(<ReactPersistRoute {...props} />);
+
+      jest.runAllTimers();
+
+      // Change route.
+      emit(getLocation("https://mypwa/u/1#input"));
+      jest.runAllTimers();
+
+      expect(props.adapter.onSave).toMatchSnapshot();
+    });
+  });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,96 @@
 import React from "react";
 
-export default class ReactPersistRoute extends React.Component {
+export type Action = "PUSH" | "POP" | "REPLACE";
+export type UnregisterCallback = () => void;
+export type LocationListener = (location: Location, action: Action) => void;
+
+export interface History {
+  listen(listener: LocationListener): UnregisterCallback;
+  push(path: string): void;
+}
+
+export type SerializableLocation = Pick<
+  Location,
+  | "hash"
+  | "host"
+  | "hostname"
+  | "href"
+  | "origin"
+  | "pathname"
+  | "port"
+  | "protocol"
+  | "search"
+>;
+
+export interface PersistenceAdapter {
+  getKey: () => string;
+  onSave: (key: string, location: SerializableLocation) => Promise<string>;
+  onLoad: (key: string) => Promise<SerializableLocation | null>;
+  onClear: (key: string) => Promise<void>;
+}
+
+export interface Props<H extends History> {
+  history: H;
+  adapter: PersistenceAdapter;
+}
+
+export default class ReactPersistRoute<
+  H extends History
+> extends React.Component<Props<H>> {
+  private _isMounted = true;
+  private _unlisten?: UnregisterCallback;
+
+  async componentDidMount() {
+    const {
+      history,
+      adapter: { getKey, onLoad }
+    } = this.props;
+
+    const location = await onLoad(getKey());
+
+    if (this._isMounted) {
+      if (location) {
+        history.push(location.href);
+      }
+
+      this._unlisten = history.listen(this.onRouteChanged);
+    }
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+    this._unlisten && this._unlisten();
+  }
+
+  onRouteChanged: LocationListener = ({
+    hash,
+    host,
+    hostname,
+    href,
+    origin,
+    pathname,
+    port,
+    protocol,
+    search
+  }) => {
+    const {
+      adapter: { getKey, onSave }
+    } = this.props;
+
+    onSave(getKey(), {
+      hash,
+      host,
+      hostname,
+      href,
+      origin,
+      pathname,
+      port,
+      protocol,
+      search
+    });
+  };
+
   render() {
-    return <div>Hello</div>;
+    return <React.Fragment>{this.props.children}</React.Fragment>;
   }
 }
